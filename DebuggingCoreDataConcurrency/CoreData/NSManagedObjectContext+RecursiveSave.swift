4ba650617b27e9c8e9bbf92deaef1fd935e36bc7
Pawan Poudel <pawan.poudel@gmail.com>
2015-06-29 00:18:45 -0400

    Reformat code so that it fits in the blog

--- DebuggingCoreDataConcurrency/CoreData/NSManagedObjectContext+RecursiveSave.swift
+++ DebuggingCoreDataConcurrency/CoreData/NSManagedObjectContext+RecursiveSave.swift
@@ -1,22 +1,22 @@
 import CoreData
 
 extension NSManagedObjectContext {
-    func saveRecursively() {
-        performBlockAndWait {
-            if self.hasChanges {
-                self.saveThisAndParentContexts()
-            }
-        }
+  func saveRecursively() {
+    performBlockAndWait {
+      if self.hasChanges {
+        self.saveThisAndParentContexts()
+      }
     }
+  }
+  
+  func saveThisAndParentContexts() {
+    var error: NSError? = nil
+    let successfullySaved = save(&error)
     
-    func saveThisAndParentContexts() {
-        var error: NSError? = nil
-        let successfullySaved = save(&error)
-        
-        if successfullySaved {
-            parentContext?.saveRecursively()
-        } else {
-            println("Unable to save managed object context due to error: \(error!.localizedDescription)")
-        }
+    if successfullySaved {
+      parentContext?.saveRecursively()
+    } else {
+      println("Error: \(error!.localizedDescription)")
     }
+  }
 }
--- DebuggingCoreDataConcurrency/ViewController.swift
+++ DebuggingCoreDataConcurrency/ViewController.swift
@@ -2,29 +2,37 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        createUserInBackground()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    createUserInBackground()
+  }
+  
+  private func createUserInBackground() {
+    let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+    dispatch_async(dispatch_get_global_queue(priority, 0)) {
+      self.createUser()
     }
+  }
+  
+  private func createUser() {
+    let coreDataStack = CoreDataStack()
     
-    private func createUserInBackground() {
-        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
-        dispatch_async(dispatch_get_global_queue(priority, 0)) {
-            self.createUser()
-        }
-    }
-    
-    private func createUser() {
-        let coreDataStack = CoreDataStack()
+    if let newPrivateQueueContext =
+      coreDataStack.newPrivateQueueContext()
+    {
+      newPrivateQueueContext.performBlock {
+        let newUser =
+          NSEntityDescription
+            .insertNewObjectForEntityForName("User",
+              inManagedObjectContext: newPrivateQueueContext)
+                as! User
         
-        if let mainQueueContext = coreDataStack.mainQueueContext {
-            let newUser = NSEntityDescription.insertNewObjectForEntityForName("User",
-                inManagedObjectContext: mainQueueContext) as! User
-            newUser.name = "The Dude"
-            newUser.email = "dude@rubikscube.com"
-            
-            mainQueueContext.saveRecursively()
-        }
+        newUser.name = "The Dude"
+        newUser.email = "dude@rubikscube.com"
+        
+        newPrivateQueueContext.saveRecursively()
+      }
     }
+  }
 }
